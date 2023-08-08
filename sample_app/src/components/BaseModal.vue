229f01d6d105c8b45e0f03b47fd733ccf6c223e7
bimal <bimal@OKICOM>
2023-08-08 14:20:59 +0900

    added input text field for searching city

--- sample_app/src/components/BaseModal.vue
+++ sample_app/src/components/BaseModal.vue
@@ -9,7 +9,8 @@ defineProps({
 </script>
 
 <template>
-    <Transition name="modal-outer">
+  <Teleport to="body">
+      <Transition name="modal-outer">
     <div v-show="modalActive" class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8" >
         <Transition name="modal-inner">
         <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md" >
@@ -21,6 +22,7 @@ defineProps({
         </Transition>
     </div>
     </Transition>
+  </Teleport>
 </template>
 
 <style scoped>
